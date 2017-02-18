63ddd20c34a574a8162ccc21880d708d37ef8635
May Khine <maykhine@nexlabs.co>
2017-02-18 22:50:50 +0630

    modify match's active_admin

--- app/admin/match.rb
+++ app/admin/match.rb
@@ -5,4 +5,35 @@ ActiveAdmin.register Match do
 #
 permit_params :team1_id, :team2_id, :winning_team_id
 
+  index do
+    selectable_column
+    column :id
+    column :team1
+    column :team2
+    column :winning_team
+    actions
+  end
+
+  filter :team1
+  filter :team2
+  filter :winning_team
+
+  #for show page
+  show do |a|
+    attributes_table do
+      row :team1
+      row :team2
+      row :winning_team
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :team1
+      f.input :team2
+      f.input :winning_team
+    end
+    f.actions
+  end
+
 end
